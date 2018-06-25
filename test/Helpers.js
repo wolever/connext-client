424e4edfe98aeac07400c71dde784006fac4641a
LayneHaber <layne.haber@gmail.com>
2018-06-25 09:46:49 -0700

    add retry fn helpers

--- test/Helpers.js
+++ test/Helpers.js
@@ -21,3 +21,25 @@ export const createRandomBytes65 = async () => {
 export const createFakeWeb3 = (eth, utils) => {
   return fakeWeb3
 }
+
+export const retry = (retries, fn) => {
+  return fn().catch(
+    err => (retries > 1 ? retry(retries - 1, fn) : Promise.reject(err))
+  )
+}
+
+export const pause = duration => {
+  return new Promise(res => setTimeout(res, duration))
+}
+
+export const backoff = (retries, fn, delay = 500) => {
+  console.log(retries)
+  console.log(fn)
+  console.log(typeof fn)
+  return fn().catch(
+    err =>
+      (retries > 1
+        ? pause(delay).then(() => backoff(retries - 1, fn, delay * 2))
+        : Promise.reject(err))
+  )
+}
