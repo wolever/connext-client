beab3dc3aaa4abb2234aad4329347a9b1c20a67d
Rahul <rksethuram9@gmail.com>
2018-06-28 13:58:01 -0700

    Add TS bindings

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,48 @@
-import {BigNumber} from 'bignumber.js'
+import { BigNumber } from "bignumber.js";
 
 export = Connext;
 
 declare class Connext {
-  constructor (opts: Connext.ConnextOptions);
+  constructor(opts: Connext.ConnextOptions);
 
-  register (initialDeposit: BigNumber): Promise<any>;
+  register(initialDeposit: BigNumber): Promise<any>;
 
-  deposit (amount: BigNumber): Promise<any>;
+  deposit(amount: BigNumber): Promise<any>;
 
-  withdraw (): Promise<any>;
+  withdraw(): Promise<any>;
 
-  withdrawFinal (): Promise<any>;
+  withdrawFinal(): Promise<any>;
 
   checkpoint(): Promise<any>;
 
-  openChannel (opts: Connext.OpenChannelOptions): Promise<any>;
+  openChannel(opts: Connext.OpenChannelOptions): Promise<any>;
 
-  joinChannel (channelId: string): Promise<any>;
+  joinChannel(channelId: string): Promise<any>;
 
-  updateBalance (opts: Connext.UpdateBalanceOptions): Promise<any>;
+  updateBalance(opts: Connext.UpdateBalanceOptions): Promise<any>;
 
-  cosignBalanceUpdate (opts: Connext.CosignBalanceUpdateOptions): Promise<string>;
+  cosignBalanceUpdate(
+    opts: Connext.CosignBalanceUpdateOptions
+  ): Promise<string>;
 
-  fastCloseChannel (channelId: string): Promise<any>;
+  fastCloseChannel(channelId: string): Promise<any>;
 
-  closeChannel (channelId: string): Promise<any>;
+  closeChannel(channelId: string): Promise<any>;
 
-  closeChannels (channels: Connext.Channel[]): Promise<any>;
+  closeChannels(channels: Connext.Channel[]): Promise<any>;
+
+  static createLCStateUpdateFingerprint(
+    opts: Connext.LcUpdateFingerprint
+  ): string;
+
+  static recoverSignerFromLCStateUpdate(opts: Connext.RecoverLcUpdate): string;
+
+  static createVCStateUpdateFingerprint(
+    opts: Connext.VcUpdateFingerprint
+  ): string;
+
+  static recoverSignerFromVCStateUpdate(opts: Connext.RecoverVcUpdate): string;
 }
-
 
 declare namespace Connext {
   export interface ConnextOptions {
@@ -54,6 +67,35 @@ declare namespace Connext {
   export interface CosignBalanceUpdateOptions {
     channelId: string;
     balance: BigNumber;
+    sig: string;
+  }
+
+  export interface LcUpdateFingerprint {
+    isClose: boolean;
+    lcId: string;
+    nonce: number;
+    openVcs: number;
+    vcRootHash: string;
+    partyA: string;
+    partyI: string;
+    balanceA: BigNumber;
+    balanceI: BigNumber;
+  }
+
+  export interface RecoverLcUpdate extends LcUpdateFingerprint {
+    sig: string;
+  }
+
+  export interface VcUpdateFingerprint {
+    channelId: string;
+    nonce: number;
+    partyA: string;
+    partyB: string;
+    balanceA: BigNumber;
+    balanceB: BigNumber;
+  }
+
+  export interface RecoverVcUpdate extends VcUpdateFingerprint {
     sig: string;
   }
 
