5578f5c909628d192eb107b854335eac703a76b4
LayneHaber <layne.haber@gmail.com>
2018-07-01 18:17:03 -0700

    add contract errors

--- src/helpers/Errors.js
+++ src/helpers/Errors.js
@@ -28,3 +28,26 @@ export class ParameterValidationError extends Error {
     Error.captureStackTrace(this, ParameterValidationError)
   }
 }
+
+export class ContractError extends Error {
+  constructor (...args) {
+    super(...args)
+    this.name = this.constructor.name
+    this.methodName = args[0]
+    if (args.length === 4) {
+      // [methodName, statusCode, transactionHash, message]
+      this.statusCode = args[1]
+      this.transactionHash = args[2]
+      this.message = `[${args[1]}: ${args[0]}] ${args[3]}. Tx: ${args[2]}`
+    } else if (args.length === 3) {
+      // [methodName, statusCode, message]
+      this.statusCode = args[1]
+      this.transactionHash = args[2]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length === 2) {
+      // [methodName, message]
+      this.statusCode = 300
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+  }
+}
