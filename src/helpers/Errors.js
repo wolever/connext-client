25eb7c0a9be56932f5a932a66b7a635c4292b4fa
LayneHaber <layne.haber@gmail.com>
2018-07-01 22:08:41 -0700

    add other error types

--- src/helpers/Errors.js
+++ src/helpers/Errors.js
@@ -1,8 +1,97 @@
-export class ChannelOpenError extends Error {
+export class VCCloseError extends Error {
   constructor (...args) {
     // [methodName, statusCode, message]
     super(args)
-    this.name = this.constructor.name
+    this.name = 'ChannelCloseError'
+    if (args.length === 3) {
+      this.methodName = args[0]
+      this.statusCode = args[1]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length == 2) {
+      this.methodName = args[0]
+      this.statusCode = 650
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+    Error.captureStackTrace(this, VCCloseError)
+  }
+}
+
+export class LCCloseError extends Error {
+  constructor (...args) {
+    // [methodName, statusCode, message]
+    super(args)
+    this.name = 'ChannelCloseError'
+    if (args.length === 3) {
+      this.methodName = args[0]
+      this.statusCode = args[1]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length == 2) {
+      this.methodName = args[0]
+      this.statusCode = 600
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+    Error.captureStackTrace(this, LCCloseError)
+  }
+}
+
+export class VCUpdateError extends Error {
+  constructor (...args) {
+    // [methodName, statusCode, message]
+    super(args)
+    this.name = 'UpdateStateError'
+    if (args.length === 3) {
+      this.methodName = args[0]
+      this.statusCode = args[1]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length == 2) {
+      this.methodName = args[0]
+      this.statusCode = 550
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+    Error.captureStackTrace(this, VCUpdateError)
+  }
+}
+
+export class LCUpdateError extends Error {
+  constructor (...args) {
+    // [methodName, statusCode, message]
+    super(args)
+    this.name = 'UpdateStateError'
+    if (args.length === 3) {
+      this.methodName = args[0]
+      this.statusCode = args[1]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length == 2) {
+      this.methodName = args[0]
+      this.statusCode = 500
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+    Error.captureStackTrace(this, LCUpdateError)
+  }
+}
+
+export class VCOpenError extends Error {
+  constructor (...args) {
+    // [methodName, statusCode, message]
+    super(args)
+    this.name = 'ChannelOpenError'
+    this.methodName = args[0]
+    if (args.length === 3) {
+      this.statusCode = args[1]
+      this.message = `[${args[1]}: ${args[0]}] ${args[2]}`
+    } else if (args.length == 2) {
+      this.statusCode = 450
+      this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
+    }
+    Error.captureStackTrace(this, VCOpenError)
+  }
+}
+
+export class LCOpenError extends Error {
+  constructor (...args) {
+    // [methodName, statusCode, message]
+    super(args)
+    this.name = 'ChannelOpenError'
     if (args.length === 3) {
       this.methodName = args[0]
       this.statusCode = args[1]
@@ -12,20 +101,7 @@ export class ChannelOpenError extends Error {
       this.statusCode = 400
       this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
     }
-    Error.captureStackTrace(this, ChannelOpenError)
-  }
-}
-
-export class ParameterValidationError extends Error {
-  constructor (...args) {
-    // [methodName, variableName, validatorResponse]
-    super(...args)
-    this.name = this.constructor.name
-    this.statusCode = 200
-    this.methodName = args[0]
-    this.variableName = args[1]
-    this.message = `[${args[0]}][${args[1]}] : ${args[2]}`
-    Error.captureStackTrace(this, ParameterValidationError)
+    Error.captureStackTrace(this, LCOpenError)
   }
 }
 
@@ -49,5 +125,18 @@ export class ContractError extends Error {
       this.statusCode = 300
       this.message = `[${this.statusCode}: ${args[0]}] ${args[1]}`
     }
+  }
+}
+
+export class ParameterValidationError extends Error {
+  constructor (...args) {
+    // [methodName, variableName, validatorResponse]
+    super(...args)
+    this.name = this.constructor.name
+    this.statusCode = 200
+    this.methodName = args[0]
+    this.variableName = args[1]
+    this.message = `[${args[0]}][${args[1]}] : ${args[2]}`
+    Error.captureStackTrace(this, ParameterValidationError)
   }
 }
