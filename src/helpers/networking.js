317cff9e78aa2a28fd03818cab612b47a4ecb175
LayneHaber <layne.haber@gmail.com>
2018-07-02 21:13:34 -0700

    add axios for testing

--- src/helpers/networking.js
+++ src/helpers/networking.js
@@ -1,38 +1,62 @@
-export const GET = 'GET';
-export const POST = 'POST';
+const axios = require('axios')
 
-module.exports = function networking(baseUrl) {
+export const GET = 'GET'
+export const POST = 'POST'
+
+module.exports = function networking (
+  baseUrl,
+  useAxios = process.env.DEV ? !process.env.DEV : false
+) {
   return {
     get,
     post
-  };
-
-  function get(url) {
-    return request(url, GET);
   }
 
-  function post(url, body) {
-    return request(url, POST, body);
+  function get (url, useAxios) {
+    return request(url, GET, useAxios)
   }
 
-  async function request(url, method, body) {
+  function post (url, body, useAxios) {
+    return request(url, POST, body, useAxios)
+  }
+
+  async function request (url, method, body, useAxios) {
     const opts = {
-      method,
-      mode: 'cors',
-      credentials: 'include'
-    };
+      method
+    }
 
     if (method === POST) {
-      opts.body = JSON.stringify(body);
+      opts.body = JSON.stringify(body)
     }
 
-    const res = await fetch(`${baseUrl}/${url}`, opts);
+    let res
+    if (useAxios === false) {
+      opts['mode'] = 'cors'
+      opts['credentials'] = 'include'
+      res = await fetch(`${baseUrl}/${url}`, opts)
+    } else {
+      opts.headers = {
+        Cookie: `hub.sid=${process.env.HUB_AUTH};`,
+        Authorization: `Bearer ${process.env.HUB_AUTH}`
+      }
+      try {
+        res = await axios(`${baseUrl}/${url}`, opts)
+      } catch (e) {
+        res = e.response
+      }
+    }
 
     if (res.status < 200 || res.status > 299) {
-      throw errorResponse(res.status, res.body, `Received non-200 response: ${res.status}`)
+      throw errorResponse(
+        res.status,
+        res.body,
+        `Received non-200 response: ${res.status}`
+      )
     }
 
-    const data = await res.json();
+    console.log(':)))')
+    const data = useAxios ? res.data : await res.json()
+    console.log(data)
 
     return {
       data
