b51d2f96f70f1268c3f00924b2120308d1049ee7
LayneHaber <layne.haber@gmail.com>
2018-07-11 12:27:40 -0700

    client skeleton, tests non functional

--- test/Disputes.test.js
+++ test/Disputes.test.js
@@ -72,7 +72,11 @@ describe('Connext dispute cases', () => {
           subchanBI = lcB.channelId
         }
         // if insufficient funds, request ingrid deposit into subchanBI
-        if (Web3.utils.toBN(lcB.balanceI).lt(initialDeposit)) {
+        if (
+          Web3.utils
+            .toBN(lcB.balanceI)
+            .lt(Web3.utils.toBN(Web3.utils.toWei('1', 'ether')))
+        ) {
           await timeout(5000)
           await client.requestIngridDeposit({
             lcId: subchanBI,
@@ -90,7 +94,7 @@ describe('Connext dispute cases', () => {
           await timeout(20000) // wait for chainsaw
         }
 
-        // create VC between partyA and partyB if doesnt exist
+        // create/update VC between partyA and partyB if doesnt exist
         vc = await client.getChannelByParties({ partyA, partyB })
         if (vc == null) {
           vcId = await client.openChannel({
@@ -99,24 +103,26 @@ describe('Connext dispute cases', () => {
             sender: partyA
           })
           vc = await client.getChannelById(vcId)
+          // update VC 3x
+          for (let i = 0; i < 3; i++) {
+            balanceA = Web3.utils.toBN(vc.balanceA)
+            balanceB = Web3.utils.toBN(vc.balanceB)
+            balanceA = balanceA.sub(
+              Web3.utils.toBN(Web3.utils.toWei('0.1', 'ether'))
+            )
+            balanceB = balanceB.add(
+              Web3.utils.toBN(Web3.utils.toWei('0.1', 'ether'))
+            )
+            await client.updateBalance({
+              channelId: vcId,
+              balanceA,
+              balanceB
+            })
+          }
         } else {
           vcId = vc.channelId
-        }
-        // update VC 3x
-        balanceA = Web3.utils.toBN(vc.balanceA)
-        balanceB = Web3.utils.toBN(vc.balanceB)
-        for (let i = 0; i < 3; i++) {
-          balanceA = balanceA.sub(
-            Web3.utils.toBN(Web3.utils.toWei('0.1', 'ether'))
-          )
-          balanceB = balanceB.add(
-            Web3.utils.toBN(Web3.utils.toWei('0.1', 'ether'))
-          )
-          await client.updateBalance({
-            channelId: vcId,
-            balanceA,
-            balanceB
-          })
+          balanceA = Web3.utils.toBN(vc.balanceA)
+          balanceB = Web3.utils.toBN(vc.balanceB)
         }
       }
     )
@@ -134,11 +140,78 @@ describe('Connext dispute cases', () => {
       expect(stub.calledOnce).to.be.true
     })
 
-    it('should call initVcStateContractHandler', async () => {})
+    it('should call initVcStateContractHandler', async () => {
+      // get initial vc state
+      let vc0 = await client.getVcInitialState(vcId)
+      // init vc on chain
+      const response = await client.initVcStateContractHandler({
+        subchanId: subchanAI, // caller subchan
+        vcId,
+        nonce: 0,
+        partyA,
+        partyB, // also in vc0 obj
+        balanceA: Web3.utils.toBN(vc0.balanceA),
+        balanceB: Web3.utils.toBN(vc0.balanceB), // should always be 0
+        sigA: vc0.sigA,
+        sender: partyA // optional, for testing
+      })
+      console.log(response)
+      // response should be tx hash
+      const tx = await client.web3.eth.getTransaction(response)
 
-    it('should call settleVCContractHandler', async () => {})
+      // assert tx was successfully submitted
+      expect(tx.from).to.equal(partyA)
+      expect(tx.to).to.equal(client.contractAddress)
+    })
 
-    it('should wait out challenge period and call closeVirtualChannelContractHandler', async () => {})
+    it('should call settleVCContractHandler', async () => {
+      // get latest vc state
+      let vcN = await client.getLatestVCStateUpdate(vcId)
+      const response = await client.settleVcContractHandler({
+        subchanId: subchanAI, // callers subchan
+        vcId,
+        nonce: vcN.nonce,
+        partyA,
+        partyB, // also in vcN obj
+        balanceA: Web3.utils.toBN(vcN.balanceA),
+        balanceB: Web3.utils.toBN(vcN.balanceB),
+        sigA: vcN.sigA,
+        sender: partyA // optional, default accounts[0]
+      })
+      // response should be tx hash
+      const tx = await client.web3.eth.getTransaction(response)
+
+      // assert tx was successfully submitted
+      expect(tx.from).to.equal(partyA)
+      expect(tx.to).to.equal(client.contractAddress)
+      // assert vc status chained
+      await timeout(15000) // wait for chainsaw to change channel status
+      vc = await client.getChannelById(vcId)
+      expect(vc.state).to.equal(2) // settling
+    })
+
+    it('should wait out challenge period and call closeVirtualChannelContractHandler', async () => {
+      // should not need to await, challenge set to 15 sec on test
+      // right now there is no way to get the challenge timeout info, but well add it like this:
+      while (vc.challengeTimeout < Date.now()) {
+        await timeout(3000)
+        vc = await client.getChannelById(vcId)
+      }
+
+      const response = await client.closeVirtualChannelContractHandler({
+        lcId: subchanAI, // senders subchan
+        vcId,
+        sender: partyA // optional, defaults to accounts[0]
+      })
+      // response should be tx hash
+      const tx = await client.web3.eth.getTransaction(response)
+
+      // assert tx was successfully submitted
+      expect(tx.from).to.equal(partyA)
+      expect(tx.to).to.equal(client.contractAddress)
+      // should also increase lcA balance on chain
+      // but this is probably broken by chainsaw atm so test l8r
+    })
 
     it('should be able to settle multiple VCs on chain', async () => {})
 
@@ -148,15 +221,67 @@ describe('Connext dispute cases', () => {
   })
 
   describe('hub did not countersign closing lc update', () => {
+    let response
     it('should call withdraw without i-countersiging closing update', async () => {
       // mock response from hub for client.fastCloseLCHandler
+      let stub = sinon.stub(client, 'fastCloseLcHandler').returns({
+        sigI: ''
+      }) // hub doesnt cosign
       // to not return lcFinal.sigI
-      const response = await client.withdraw(partyA)
-      assert.equal(response.fastClosed, false)
+      response = await client.withdraw(partyA)
+      expect(stub.calledOnce).to.be.true
+      expect(response.fastClosed).to.equal(false)
     })
-    it('should have called updateLCState on chain and sent lc into challenge state', async () => {})
+    it('should have called updateLCState on chain and sent lc into challenge state', async () => {
+      // response.response should be tx hash
+      const tx = await client.web3.eth.getTransaction(response.response)
 
-    it('should wait out challenge period and call withdrawFinal', async () => {})
+      // assert tx was successfully submitted
+      expect(tx.from).to.equal(partyA)
+      expect(tx.to).to.equal(client.contractAddress)
+
+      await timeout(20000) // wait for chainsaw to pick up settled state change
+      lcA = await client.getLcById(subchanAI)
+      expect(lcA.state).to.equal(2) // settling
+    })
+
+    it('should wait out challenge period and call withdrawFinal', async () => {
+      // already awaited, should not have to again
+      while (lcA.challengeTimeout < Date.now()) {
+        await timeout(3000)
+        lcA = await client.getLcById(subchanAI)
+      }
+      // get previous balances
+      const prevBalA = await client.web3.eth.getBalance(partyA)
+      const prevBalI = await client.web3.eth.getBalance(ingridAddress)
+
+      const response = await client.withdrawFinal(partyA)
+      const tx = await client.web3.eth.getTransaction(response.response)
+
+      // assert tx was successfully submitted
+      expect(tx.from).to.equal(partyA)
+      expect(tx.to).to.equal(client.contractAddress)
+      expect(response).to.equal()
+      // assert account balances increased
+      const expectedA = Web3.utils.fromWei(
+        Web3.utils.toBN(lcA.balanceA).add(Web3.utils.toBN(prevBalA)),
+        'ether'
+      )
+      const expectedI = Web3.utils.fromWei(
+        Web3.utils.toBN(lcA.balanceI).add(Web3.utils.toBN(prevBalI)),
+        'ether'
+      )
+      const finalBalA = Web3.utils.fromWei(
+        await client.web3.eth.getBalance(partyA),
+        'ether'
+      )
+      const finalBalI = Web3.utils.fromWei(
+        await client.web3.eth.getBalance(ingridAddress),
+        'ether'
+      )
+      expect(Math.round(expectedA)).to.equal(Math.round(finalBalA))
+      expect(Math.round(expectedI)).to.equal(Math.round(finalBalI))
+    })
   })
 
   describe('partyA takes VC to chain with earlier nonce', () => {
