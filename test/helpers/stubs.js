0e5a4ce6d4b0c106f5c55f979c42a59f3875c0ec
LayneHaber <layne.haber@gmail.com>
2018-08-01 23:16:27 -0700

    update hub for deposit

--- test/helpers/stubs.js
+++ test/helpers/stubs.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon')
 const nock = require('nock')
 const Web3 = require('web3')
+const Connext = require('../../src/Connext')
 
 export function createStubbedContract () {
   const sendTxStub = {
@@ -25,11 +26,14 @@ export function createStubbedContract () {
   return contractMethods
 }
 
-export async function createStubbedHub (baseUrl) {
+export async function createStubbedHub (baseUrl, type) {
   const web3 = new Web3('http://localhost:8545')
   const accounts = await web3.eth.getAccounts()
   const ingridAddress = accounts[0]
   const partyA = accounts[1]
+
+  const channelId1 =
+    '0x1000000000000000000000000000000000000000000000000000000000000000'
 
   let stubHub = nock(baseUrl).persist(true)
   // get challenge timer
@@ -41,13 +45,46 @@ export async function createStubbedHub (baseUrl) {
       challenge: 3600
     })
   // get open ledger channels
-  stubHub
-    .get(`/ledgerchannel/a/${partyA.toLowerCase()}?status=LCS_OPENED`)
-    .reply(200, {
-      data: []
-    })
+  switch (type) {
+    case 'OPEN_LC':
+      stubHub
+        .get(`/ledgerchannel/a/${partyA.toLowerCase()}?status=LCS_OPENED`)
+        .reply(200, [
+          {
+            channelId: '0x1000000000000000000000000000000000000000000000000000000000000000',
+            partyA: partyA.toLowerCase(),
+            partyI: ingridAddress.toLowerCase(),
+            state: 'LCS_OPENED',
+            balanceA: Web3.utils.toWei('5', 'ether'),
+            balanceI: '0',
+            nonce: 0,
+            openVcs: 0,
+            vcRootHash: Connext.generateVcRootHash({ vc0s: [] })
+          }
+        ])
+      break
+    case 'NO_LC':
+      stubHub
+        .get(`/ledgerchannel/a/${partyA.toLowerCase()}?status=LCS_OPENED`)
+        .reply(200, {
+          data: []
+        })
+      break
+    default:
+      break
+  }
 
-  stubHub.get().reply()
+  stubHub.get(`/ledgerchannel/${channelId1}`).reply(200, {
+    channelId: '0x1000000000000000000000000000000000000000000000000000000000000000',
+    partyA: partyA.toLowerCase(),
+    partyI: ingridAddress.toLowerCase(),
+    state: 'LCS_OPENED',
+    balanceA: Web3.utils.toWei('5', 'ether'),
+    balanceI: '0',
+    nonce: 0,
+    openVcs: 0,
+    vcRootHash: Connext.generateVcRootHash({ vc0s: [] })
+  })
 
   return stubHub
 }
